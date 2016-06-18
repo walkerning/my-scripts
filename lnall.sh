403f5ac6c785cfa9218cf63f721bb4bab01470ea
Xue ning <foxdoraame@gmail.com>
2016-06-19 02:54:34 +0800

    Update lnall.sh

--- lnall.sh
+++ lnall.sh
@@ -4,8 +4,8 @@ for x in `ls ./*.py`; do
     if [[ ${x} != 'setup.py' ]]; then
         filename=`basename ${x}`
         filename_withoutext="${filename%.*}"
-        rm -f ~/bin/${filename_withoutext}
-        ln -s $(pwd)/${x} ~/bin/${filename_withoutext}
-        echo 'Soft linking ' ${x} ' to ~/bin'
+        rm -f ${HOME}/.local/bin/${filename_withoutext}
+        ln -s $(pwd)/${x} ${HOME}/.local/bin/${filename_withoutext}
+        echo 'Soft linking ' ${x} ' to ~/.local/bin'
     fi
 done
